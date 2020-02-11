f3e9bbc690ea24e2a573ff0d4c64aa87056e5533
Kyler Johnson <kyler.johnson@bullhorn.com>
2020-02-11 09:55:10 -0500

    add service, components, seed data, and styles for simple table example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit, AfterViewInit } from '@angular/core';
+import { BookService } from './book.service';
+import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { Book } from './book';
+import { tap, map } from 'rxjs/operators';
+import { DataSource } from '@angular/cdk/table';
+import { CollectionViewer } from '@angular/cdk/collections';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ng-mat-grid-example';
+export class AppComponent implements OnInit {
+
+  books$: Observable<Book[]>;
+
+  constructor(private bookService: BookService) {}
+
+  ngOnInit() {
+    this.books$ = this.bookService.getBooks();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule, MatPaginatorModule, MatSortModule, MatInputModule } from '@angular/material';
+import { CdkTableModule } from '@angular/cdk/table';
+import { GridComponent } from './grid/grid.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GridComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    CdkTableModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
